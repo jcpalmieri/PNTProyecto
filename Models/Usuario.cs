ddf1d166cba508d8e71f9a034f629cabb8c3e24f
Pedro <PbelosoBerdun@gmail.com>
2024-06-13 16:28:59 -0300

    "Se agreganron las clases Usuario.cs y UsuarioDbContext.cs", no me esta dejando agregar los paquetes nugget y continuar con la guia".

---
+++ Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace PNTProyecto.Models
+{
+	public Usuario()
+	{
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string email { get; set; }
+        public string nombreUsuario {  get; set; }
+	    public string nroTelefono { get; set; }
+        public string Imagen { get; set; }
+}
+}
