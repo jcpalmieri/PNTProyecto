a90feb80bc620d3015ce14f96080b033820ba2c1
Pedro <PbelosoBerdun@gmail.com>
2024-06-14 14:16:02 -0300

    ---Nuevo-- realicé lo que Agustín me pedió
    aporte algunos botones funcionales (los del video)
    
    --Revisar--
    
    Chequeen el cambiar de appsettings.json el nombre de mi server al suyo.

--- obj/Release/net8.0/PNTProyecto.AssemblyInfo.cs
+++ obj/Release/net8.0/PNTProyecto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PNTProyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2133ff236ccb176dc11b26b059bd9a6f55871883")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16dbd3ed2e62cf21197670bdbf82fdbc3687d3c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PNTProyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PNTProyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
