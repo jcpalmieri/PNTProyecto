255e418b635711cb44d42f0e133ec1b4872b507a
Pedro <PbelosoBerdun@gmail.com>
2024-06-16 23:14:16 -0300

    x

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -5,16 +5,18 @@ namespace PNTProyecto.Models
     public class Usuario
     {
         public int UsuarioId { get; set; }
+
         [Required]
-        public string Nombre { get; set; }
-        [Required]
-        public string Apellido { get; set; }
+        public string NombreUsuario { get; set; }
+
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+
         [Required]
         [StringLength(100)]
         public string Contraseña { get; set; }
+
         [Required]
         public string Teléfono { get; set; }
     }
