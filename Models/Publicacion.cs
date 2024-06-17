92d7048da2d83aa75cc062a981caa79bc8f3527f
Pedro <PbelosoBerdun@gmail.com>
2024-06-17 14:02:08 -0300

    x

--- Models/Publicacion.cs
+++ Models/Publicacion.cs
@@ -6,9 +6,9 @@ namespace PNTProyecto.Models
     {
         [Key]
         public int nroPublicacion { get; set; }
-        [Required(ErrorMessage = "El nombre de la mascota es requerido.")]
+        
         public string nombreMascota { get; set; }
-        [Required(ErrorMessage = "La descripción es requerida.")]
+        
         public string descripcion { get; set; }
 
         public string imagen { get; set; }
