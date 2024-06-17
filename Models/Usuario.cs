687556d97c441b8b46069141a6878f541749f532
Pedro <PbelosoBerdun@gmail.com>
2024-06-17 03:21:32 -0300

    cambios

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -5,19 +5,9 @@ namespace PNTProyecto.Models
     public class Usuario
     {
         public int UsuarioId { get; set; }
-
-        [Required]
         public string NombreUsuario { get; set; }
-
-        [Required]
-        [EmailAddress]
         public string Email { get; set; }
-
-        [Required]
-        [StringLength(100)]
         public string Contraseña { get; set; }
-
-        [Required]
         public string Teléfono { get; set; }
     }
 }
--- obj/Release/net8.0/PNTProyecto.AssemblyInfo.cs
+++ obj/Release/net8.0/PNTProyecto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PNTProyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b500f0a19163fece4462a3fa49e933b723583ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a46339594a41033010db481e91ad60da8252dfad")]
 [assembly: System.Reflection.AssemblyProductAttribute("PNTProyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PNTProyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
