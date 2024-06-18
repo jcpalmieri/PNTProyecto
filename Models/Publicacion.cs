527935c5c6f39aa428e0b678c188f9a305f72adf
Pedro <PbelosoBerdun@gmail.com>
2024-06-18 10:53:19 -0300

    x

--- Models/Publicacion.cs
+++ Models/Publicacion.cs
@@ -1,29 +1,13 @@
 using System.ComponentModel.DataAnnotations;
-
 namespace PNTProyecto.Models
 {
     public class Publicacion
-    {
-        [Key]
-        public int NroPublicacion { get; set; }
-
-        [Required]
-        [Display(Name = "Nombre de la Mascota")]
-        public string NombreMascota { get; set; }
-
-        [Display(Name = "Imagen")]
-        public string Imagen { get; set; }
-
-        [Required]
-        [Display(Name = "Descripción")]
-        public string Descripcion { get; set; }
-
-        [Required]
-        [Display(Name = "Tipo de Mascota")]
-        public string TipoMascota { get; set; }
-
-        [Required]
-        [Display(Name = "Contacto")]
-        public string Contacto { get; set; }
-    }
+ {
+    public int nroPublicacion { get; set; }
+    public string NombreMascota { get; set; }
+    public string Imagen { get; set; }
+    public string TipoMascota { get; set; } 
+    public string Descripcion { get; set; }
+    public string Contacto { get; set; }
+ }
 }
