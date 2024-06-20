70b9c1b868fd7ae4a2690a56691a765a47266efd
Juan Palmieri <juancpalmieri@outlook.com>
2024-06-19 21:34:28 -0300

    cambios perfil de usuario

---
+++ Controllers/PerfilController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Linq;
+using System;
+using PNTProyecto.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+public class PerfilController : Controller
+{
+    public IActionResult Index()
+    {
+         return View();
+
+    }
+
+}
